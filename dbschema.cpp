f87cb06786df9d05ed8a69ec2d88de79b773ba7c
theme <xenotheme@gmail.com>
2015-07-01 13:22:43 +0800

    add fields() and type() api in dbschema module.

--- dbschema.cpp
+++ dbschema.cpp
@@ -10,6 +10,32 @@ QStringList DBSchema::tables() const
     return jsondoc.object().keys();
 }
 
+QList<QString> DBSchema::fields(QString tableName) const
+{
+    QJsonArray table = jsondoc.object().value(tableName).toArray();
+    QList< QString > li;
+    for (int i = 0; i < table.size(); ++i){
+        QJsonObject field = table.at(i).toObject();
+        QJsonObject::const_iterator it = field.constBegin();
+        li.append( it.key() );
+    }
+
+    return li;
+}
+
+QString DBSchema::type(QString tableName, QString fieldName) const
+{
+    QJsonArray table = jsondoc.object().value(tableName).toArray();
+    for (int i = 0; i < table.size(); ++i){
+        QJsonObject field = table.at(i).toObject();
+        QJsonObject::const_iterator it = field.constBegin();
+        if (it.key() == fieldName ){
+            return it.value().toString();
+        }
+    }
+    return QString();
+}
+
 bool DBSchema::isReady() const
 {
     return (!jsondoc.isNull()) && (!jsondoc.isEmpty());
--- dbschema.h
+++ dbschema.h
@@ -16,6 +16,8 @@ class DBSchema : public QObject
 public:
     explicit DBSchema(QObject *parent = 0);
     QStringList tables() const;
+    QList< QString > fields(QString tableName) const;
+    QString type(QString tableName, QString fieldName) const;
     bool isReady() const;
     QString createTableSql(QString tableName) const;
 
