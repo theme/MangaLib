2cdc315670e35c8df07e8378e5f19a275a8425a4
theme <xenotheme@gmail.com>
2015-07-04 12:08:44 +0800

    WIP: add a column.

--- fsmixdbmodel.cpp
+++ fsmixdbmodel.cpp
@@ -1,7 +1,44 @@
 #include "fsmixdbmodel.h"
+#include <QDebug>
 
-FSmixDBmodel::FSmixDBmodel(QObject *parent)
+FSmixDBmodel::FSmixDBmodel(QObject *parent):
+    QFileSystemModel(parent)
 {
+    // how many coloums are there in parent model?
+    qDebug() << QFileSystemModel::columnCount();
+}
 
+int FSmixDBmodel::rowCount(const QModelIndex &parent) const
+{
+    return QFileSystemModel::rowCount(parent);
+}
+
+int FSmixDBmodel::columnCount(const QModelIndex &parent) const
+{
+    return QFileSystemModel::columnCount(parent) +1;
+}
+
+QVariant FSmixDBmodel::data(const QModelIndex &index, int role) const
+{
+    if (role == Qt::DisplayRole){
+        switch (index.column()){
+        case 4:
+            return QVariant(QString("rank!?!?"));
+            break;
+        }
+    }
+    return QFileSystemModel::data(index, role);
+}
+
+QVariant FSmixDBmodel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (orientation == Qt::Orientation::Horizontal && role == Qt::DisplayRole){
+        switch (section){
+        case 4:
+            return QVariant(QString("rank!?!?"));
+            break;
+        }
+    }
+    return QFileSystemModel::headerData(section, orientation, role);
 }
 
--- fsmixdbmodel.h
+++ fsmixdbmodel.h
@@ -9,6 +9,11 @@ class FSmixDBmodel : public QFileSystemModel
     Q_OBJECT
 public:
     FSmixDBmodel(QObject *parent = 0);
+
+    int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const;
+    QVariant data(const QModelIndex &index, int role) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
 };
 
 #endif // FSMIXDBMODEL_H
